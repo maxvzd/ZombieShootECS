50d54727d133a359b4935407c74d64caee193931
maxvzd <153630971+maxvzd@users.noreply.github.com>
2024-10-01 15:36:53 +0100

    applied root motion to zombies - needs a bit more tweaking

--- Assets/Scripts/Constants.cs
+++ Assets/Scripts/Constants.cs
@@ -3,6 +3,7 @@
     public static readonly string VerticalKey = "Vertical";
     public static readonly string HorizontalKey = "Horizontal";
     public static readonly string Speed = "Speed";
+    public static readonly string IsMoving = "IsMoving";
     
     public static readonly string Fire1Key = "Fire1";
     public static readonly string Fire2Key = "Fire2";
