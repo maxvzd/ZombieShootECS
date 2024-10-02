3eb6040bbb6549c2156d290fde7939b1781063ba
maxvzd <153630971+maxvzd@users.noreply.github.com>
2024-10-02 16:15:36 +0100

    Improved moving with rootmotion for nav mesh agents

---
+++ Assets/Scripts/Zombie/ApplyRootMotionToNavMeshAgent.cs
@@ -0,0 +1,59 @@
+﻿using UnityEngine;
+using UnityEngine.AI;
+
+namespace Zombie
+{
+    public class ApplyRootMotionToNavMeshAgent : MonoBehaviour
+    {
+        private NavMeshAgent _navMeshAgent;
+        private Animator _animator;
+
+        private const float WALK_SPEED = 1f;
+        private const float RUN_SPEED = 2f;
+
+        private float _t;
+        private float _speed;
+        
+        private void Start()
+        {
+            _navMeshAgent = GetComponent<NavMeshAgent>();
+            _navMeshAgent.updatePosition = false;
+            _navMeshAgent.updateRotation = true;
+
+            _animator = GetComponent<Animator>();
+            _animator.applyRootMotion = true;
+        }
+        
+        private void Update()
+        {
+            SynchroniseAnimatorAndAgent();
+        }
+
+        private void SynchroniseAnimatorAndAgent()
+        {
+            if (_navMeshAgent.remainingDistance >= _navMeshAgent.stoppingDistance)
+            {
+                _t += Time.deltaTime;
+                _t = Mathf.Clamp(_t, 0, 1);
+            }
+            else if (_t > 0f)
+            {
+                _t -= Time.deltaTime;
+                _t = Mathf.Clamp(_t, 0, 1);
+            }
+            _speed = Mathf.Lerp(0f, WALK_SPEED, _t);
+
+            bool isMoving = _speed > 0.01f;
+            _animator.SetFloat(Constants.Speed, _speed);
+            _animator.SetBool(Constants.IsMoving, isMoving);
+        }
+
+        private void OnAnimatorMove()
+        {
+            Vector3 rootPos = _animator.rootPosition;
+            rootPos.y = _navMeshAgent.nextPosition.y;
+            transform.position = rootPos;
+            _navMeshAgent.nextPosition = rootPos;
+        }
+    }
+}
--- Assets/Scripts/Zombie/NavigateToPlayer.cs
+++ Assets/Scripts/Zombie/NavigateToPlayer.cs
@@ -1,4 +1,3 @@
-using System;
 using UnityEngine;
 using UnityEngine.AI;
 
@@ -8,67 +7,15 @@ namespace Zombie
     {
         [SerializeField] private Transform player;
         private NavMeshAgent _navMeshAgent;
-        private Animator _animator;
-
-        private Vector2 _velocity;
-        private Vector2 _smoothDeltaPos;
-    
+        
         private void Start()
         {
             _navMeshAgent = GetComponent<NavMeshAgent>();
-            _navMeshAgent.updatePosition = false;
-            _navMeshAgent.updateRotation = true;
-            
-            _animator = GetComponent<Animator>();
-            _animator.applyRootMotion = true;
         }
 
         private void Update()
         {
             _navMeshAgent.destination = player.position;
-
-            SynchroniseAnimatorAndAgent();
-        }
-
-        private void SynchroniseAnimatorAndAgent()
-        {
-            Vector3 worldDeltaPos = _navMeshAgent.nextPosition - transform.position;
-            worldDeltaPos.y = 0;
-
-            float dx = Vector3.Dot(transform.right, worldDeltaPos);
-            float dy = Vector3.Dot(transform.forward, worldDeltaPos);
-            Vector2 deltaPos = new Vector2(dx, dy);
-
-            float smooth = Mathf.Min(1, Time.deltaTime / 0.1f);
-            _smoothDeltaPos = Vector2.Lerp(_smoothDeltaPos, deltaPos, smooth);
-            _velocity = _smoothDeltaPos / Time.deltaTime;
-
-            if (_navMeshAgent.remainingDistance <= _navMeshAgent.stoppingDistance)
-            {
-                _velocity = Vector2.Lerp(Vector2.zero, _velocity, _navMeshAgent.remainingDistance);
-            }
-
-            bool isMoving = _navMeshAgent.velocity.magnitude > 0f && _navMeshAgent.remainingDistance > _navMeshAgent.stoppingDistance;
-            
-            _animator.SetFloat(Constants.Speed, _velocity.magnitude);
-            _animator.SetBool(Constants.IsMoving, isMoving);
-            
-            // float deltaMagnitude = worldDeltaPos.magnitude;
-            // if (deltaMagnitude > _navMeshAgent.radius / 2f)
-            // {
-            //     transform.position = Vector3.Lerp(
-            //         _animator.rootPosition,
-            //         _navMeshAgent.nextPosition,
-            //         smooth);
-            // }
-        }
-
-        private void OnAnimatorMove()
-        {
-            Vector3 rootPos = _animator.rootPosition;
-            rootPos.y = _navMeshAgent.nextPosition.y;
-            transform.position = rootPos;
-            _navMeshAgent.nextPosition = rootPos;
         }
     }
 }
